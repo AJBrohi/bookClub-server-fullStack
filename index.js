6d4ad9b43188d046ac4044fa91a63d9fb3c042af
Al-Junaed Islam <brohi96@gmail.com>
2021-04-19 06:15:45 +0600

    server updated

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(cors());
 
 const port = 5000;
 
-// console.log(process.env.DB_USER);
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
@@ -38,30 +37,23 @@ client.connect(err => {
   })
 
   app.get('/allBooks', (req, res) => {
-    // console.log(204)
     const search = req.query.search;
-    // console.log(search," => ");
     bookCollection.find({bookName: {$regex: search}})
       .toArray((err, documents) => {
-        // console.log(documents);
         res.send(documents);
       })
   })
 
-  // Delete a book from MongoDB by id using (params)...
   app.get('/delete/:id', (deleteReq, deleteRes) => {
     const id = deleteReq.params.id;
     bookCollection.deleteOne({ _id: ObjectId(id) })
       .then(document => {
-        // console.log(document.deletedCount);
         deleteRes.send(document);
       })
   })
 
-  // User Collections
   app.get('/delete/user-book/:id', (req, res) => {
     const id = req.params.id;
-    // console.log(id)
     userBookCollection.deleteOne({ _id: ObjectId(id) })
       .then(document => {
         res.send(document);
@@ -77,20 +69,16 @@ client.connect(err => {
 
   app.get('/user-books', (req, res) => {
     const userEmail = req.query.email;
-    // console.log(userEmail);
     userBookCollection.find({ userEmail })
       .toArray((err, documents) => {
-        // console.log(documents);
         res.send(documents);
       })
   })
 
   app.get('/delete/user-books/email', (req, res) => {
     const userEmail = req.query.email;
-    // console.log(userEmail);
     userBookCollection.deleteMany({ userEmail })
       .then(document => {
-        // console.log(document.deletedCount);
         res.send(document);
       })
   })
@@ -98,19 +86,16 @@ client.connect(err => {
   app.post('/user-book/update-from-table/:id', (req, res) => {
     const id = req.params.id;
     const quantity = req.body.quantity;
-    // console.log( quantity, " ", id)
     userBookCollection.updateOne({ _id: ObjectId(id) }, {
       $set: { quantity }
     })
       .then(result => {
-        // console.log(result.modifiedCount);
         res.send(result);
       })
   })
 
   app.post('/user/add-book', (req, res) => {
     const userBookData = req.body;
-    // console.log(userBookData)
     userBookCollection.insertOne(userBookData)
       .then(data => {
         res.send(data.ops[0])
@@ -119,16 +104,12 @@ client.connect(err => {
   })
 
   app.post('/user-book/update-from-book-card/:id', (req, res) => {
-    // console.log(req.params.id,"\n",req.body);
     const id = req.params.id;
-    // console.log(id);
     const userEmail = req.query.email;
     const userBookData = req.body;
     userBookCollection.findOne({ _id: ObjectId(id), userEmail })
       .then(data => {
-        // console.log("Old data => ",  data);
         data.quantity += userBookData.quantity;
-        // console.log("Updated data => ", data);
         userBookCollection.updateOne({ _id: ObjectId(id) }, {
           $set: { quantity: data.quantity }
         })
@@ -139,22 +120,18 @@ client.connect(err => {
       .catch(err => console.log(err))
   })
 
-  // Order Collection
   app.get('/user-order', (req, res) => {
     const email = req.query.email;
     ordersCollection.findOne({ email })
       .then(data => {
-        // console.log(data, " =>");
         res.send(data);
       })
   })
 
   function orderInsertion(order, email, res) {
 
-    // console.log(email)
     ordersCollection.insertOne({ order, email })
       .then(data => {
-        // console.log(data.ops[0]);
         res.send(data.ops[0]);
       })
   }
@@ -162,10 +139,8 @@ client.connect(err => {
   app.post('/update-user/orders', (req, res) => {
     const order = req.body;
     const email = req.query.email;
-    // console.log(email);
     ordersCollection.findOne({ email })
       .then(data => {
-        // console.log(data);
         if (!data) {
           orderInsertion(order, email, res);
         }
@@ -174,7 +149,6 @@ client.connect(err => {
             $set: { email, order }
           })
             .then(result => {
-              // console.log(result);
               res.send(result);
             })
         }
